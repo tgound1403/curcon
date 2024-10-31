2905eb8611f9e7248da7fa326766d5729fbc9fea
tgound1403 <nguyentrieuduong14032002@gmail.com>
2024-10-31 20:02:28 +0700

    feat: add show snackbar in case press get latest rate button but no connectivity

--- app/src/main/java/com/example/curcon/feature/presentation/screen/CurrencyConverterScreen.kt
+++ app/src/main/java/com/example/curcon/feature/presentation/screen/CurrencyConverterScreen.kt
@@ -1,14 +1,19 @@
 package com.example.curcon.feature.presentation.screen
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -17,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +33,8 @@ import com.example.curcon.feature.presentation.component.AnimatedText
 import com.example.curcon.feature.presentation.component.HomeConverter
 import com.example.curcon.feature.presentation.component.RateInformation
 import com.example.curcon.feature.presentation.viewmodel.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun CurrencyConverterScreen(viewModel: MainViewModel) {
@@ -39,73 +47,96 @@ fun CurrencyConverterScreen(viewModel: MainViewModel) {
     val someCountryRates by viewModel.someCountryRate.collectAsState()
     val currencyTrends by viewModel.trends.collectAsState()
 
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
     SideEffect { viewModel.getDataFromLocal() }
 
-    Column(
-        modifier = Modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.height(12.dp))
-        AnimatedText()
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxHeight()
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column {
-                HomeConverter(
-                    currencies = supportCurrency,
-                    fromCurrency = fromCurrency,
-                    toCurrency = toCurrency,
-                    onFromCurrencyChanged = {
-                        fromCurrency = it; viewModel.getSomeCountriesCurrencyRate(it)
-                    },
-                    onToCurrencyChanged = {
-                        toCurrency = it; viewModel.convertCurrency(
-                        amount,
-                        fromCurrency,
-                        toCurrency
-                    )
-                    },
-                    amount = amount,
-                    onAmountChanged = {
-                        amount = it.toDouble(); viewModel.getSomeCountriesCurrencyRate(fromCurrency)
-                    },
-                    onChanged = {
-                        viewModel.convertCurrency(amount, fromCurrency, toCurrency)
-                    },
-                    convertedAmount = convertedAmount,
-                    onSwap = {
-                        val tempCurr = fromCurrency
-                        fromCurrency = toCurrency
-                        toCurrency = tempCurr
-                        amount = 0.0
-                        viewModel.clearModelData()
-                    }
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                if (indicativeRate != 0.0 && amount != 0.0) {
-                    Column {
-                        Box(
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(12.dp))
-                                .background(Color.LightGray)
-                                .clickable { viewModel.compareAndSaveRates() }) {
-                            Text("Get latest rates", modifier = Modifier.padding(8.dp))
-                        }
-                        Spacer(modifier = Modifier.height(8.dp))
-                        RateInformation(
+            Spacer(modifier = Modifier.height(12.dp))
+            AnimatedText()
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                Column {
+                    HomeConverter(
+                        currencies = supportCurrency,
+                        fromCurrency = fromCurrency,
+                        toCurrency = toCurrency,
+                        onFromCurrencyChanged = {
+                            fromCurrency = it; viewModel.getSomeCountriesCurrencyRate(it)
+                        },
+                        onToCurrencyChanged = {
+                            toCurrency = it; viewModel.convertCurrency(
+                            amount,
                             fromCurrency,
-                            indicativeRate,
-                            toCurrency,
-                            someCountryRates,
-                            currencyTrends
+                            toCurrency
                         )
+                        },
+                        amount = amount,
+                        onAmountChanged = {
+                            amount = it.toDouble(); viewModel.getSomeCountriesCurrencyRate(
+                            fromCurrency
+                        )
+                        },
+                        onChanged = {
+                            viewModel.convertCurrency(amount, fromCurrency, toCurrency)
+                        },
+                        convertedAmount = convertedAmount,
+                        onSwap = {
+                            val tempCurr = fromCurrency
+                            fromCurrency = toCurrency
+                            toCurrency = tempCurr
+                            amount = 0.0
+                            viewModel.clearModelData()
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    if (indicativeRate != 0.0 && amount != 0.0) {
+                        Column {
+                            Box(
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(12.dp))
+                                    .background(Color.LightGray)
+                                    .clickable {
+                                        if (!viewModel.isNetworkAvailable()) {
+                                            Log.d("NETWORK CHECK", "NO NETWORK")
+                                            scope.launch(Dispatchers.Main) {
+                                                snackbarHostState.showSnackbar(
+                                                    message = "We need internet connection to update",
+                                                    duration = SnackbarDuration.Short
+                                                )
+                                            }
+                                        } else {
+                                            viewModel.compareAndSaveRates()
+                                            Log.d("NETWORK CHECK", "NO NETWORK")
+                                        }
+                                    }) {
+                                Text("Get latest rates", modifier = Modifier.padding(8.dp))
+                            }
+                            Spacer(modifier = Modifier.height(8.dp))
+                            RateInformation(
+                                fromCurrency,
+                                indicativeRate,
+                                toCurrency,
+                                someCountryRates,
+                                currencyTrends
+                            )
+                        }
+                    } else {
+                        Text("Input amount to see more information")
                     }
-                } else {
-                    Text("Input amount to see more information")
                 }
             }
         }
-
+        SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
     }
 }
--- app/src/main/java/com/example/curcon/feature/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/curcon/feature/presentation/viewmodel/MainViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.curcon.feature.presentation.viewmodel
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.work.OneTimeWorkRequestBuilder
@@ -136,4 +140,25 @@ class MainViewModel @Inject constructor(
             _trends.value = result
         }
     }
+
+
+    fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
+    }
+
 }
